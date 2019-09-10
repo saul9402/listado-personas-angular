93af4358aaea4f7271e6816d2fa2bab61fe0f15a
savila <saul.avila@nyrhcloud.com>
2019-09-10 13:44:28 -0500

    se agrega directiva ngFor

---
+++ src/app/persona.model.ts
@@ -0,0 +1,12 @@
+export class Persona {
+    /*
+    nombre: string;
+    apellido: string;
+    constructor(nombre: string, apellido: string) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }*/
+
+    /*Sintaxis simplificada se crean los atributos y se asignan valores al mismo tiempo*/
+    constructor(public nombre: string, public apellido: string) { }
+}
