8537d349c07513abf2a92bb95ebb80999b78a331
savila <saul.avila@nyrhcloud.com>
2019-09-10 17:08:07 -0500

    se agrega un nuevo servicio para enivar mensajes a consola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Persona } from './persona.model';
+import { LogginService } from './LogginService.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,10 @@ export class AppComponent {
   titulo = 'Listado de Personas';
   personas: Persona[] = [new Persona("Juan", "Perez"), new Persona("Laura", "Juarez")];
 
+  constructor(private loggingService: LogginService) { }
+
   onPersonaAgregada(persona: Persona) {
+    this.loggingService.enviaMensajeAConsola("Se agregaó al arreglo a la siguiente persona: " + JSON.stringify(persona));
     this.personas.push(persona);
   }
 }
