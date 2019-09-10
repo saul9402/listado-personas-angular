41cf38b4659f1277ecbb4159ea8bc09b667c8b6f
savila <saul.avila@nyrhcloud.com>
2019-09-10 14:29:54 -0500

    se agrega event binding entre componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,8 @@ import { Persona } from './persona.model';
 export class AppComponent {
   titulo = 'Listado de Personas';
   personas: Persona[] = [new Persona("Juan", "Perez"), new Persona("Laura", "Juarez")];
-  nombreInput: string;
-  apellidoInput: string;
 
-
-  onAgregarPersona() {
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personas.push(persona1);
+  onPersonaAgregada(persona: Persona) {
+    this.personas.push(persona);
   }
-
 }
